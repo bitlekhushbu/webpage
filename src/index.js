7a3c8afa6a1d59b964815a6a8340282637eefa79
adexlabs <contact@adexlabs.com>
2024-02-28 13:06:07 +0530

    updated code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import { Analytics } from '@vercel/analytics/react';
 ReactDOM.render(
     <>
         <App/>
+        <Analytics/>
     </>,
     document.getElementById('root'));
